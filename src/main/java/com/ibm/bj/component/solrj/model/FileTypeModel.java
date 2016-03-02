7499dac45d6b8ed4ad3e8436008f31cbf2aa8169
zhengxiangyun <csu_zxy@csu.edu.cn>
2016-03-02 17:25:34 +0800

    Added files via upload

---
+++ src/main/java/com/ibm/bj/component/solrj/model/FileTypeModel.java
@@ -0,0 +1,27 @@
+package com.ibm.bj.component.solrj.model;
+
+/**
+ * Created by zhengxiangyun on 2015/11/5.
+ */
+public class FileTypeModel {
+
+    String typeName;
+
+    Integer typeCount;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Integer getTypeCount() {
+        return typeCount;
+    }
+
+    public void setTypeCount(Integer typeCount) {
+        this.typeCount = typeCount;
+    }
+}
